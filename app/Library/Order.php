5253cd992b03ca8fa9177c4a9947edeb5d5d09ec
Rizha <rizha.musthafa@gmail.com>
2016-01-14 19:04:09 +0700

    update sales order and add function parse order from elevenia to regional

--- app/Library/Order.php
+++ app/Library/Order.php
@@ -2,7 +2,6 @@
 
 namespace App\Library;
 
-use App\Model\Partner;
 use App\Model\SalesOrder;
 use SimpleXMLElement;
 
@@ -92,196 +91,57 @@ class Order
 		return $res;
 	}
 
-	public function parseOrder($partnerId, $order){
-		/*
-		 *
-		 * format return array nya :
-		 *
-		 * array(
-		 * 		"{URL fullfillment by ordNo ke - 1}" => {array regional put sales order},
-		 * 		"{URL fullfillment by ordNo ke - 2}" => {array regional put sales order},
-		 * 		"{URL fullfillment by ordNo ke - (n)}" => {array regional put sales order}
-		 * )
-		 *
-		 *
-		 */
-		//$partnerId = $this->getPartnerId();
-		$arrFullFillment = array();
-		$arrFullFillmentItem = array();
-
-		if(isset($order[0]))
+	public function parseOrderFromEleveniaToCmps($partnerId, $orderElev)
+	{
+		$orderItem = [];
+		foreach($orderElev['productList'] as $val)
 		{
-			foreach($order as $key => $data)
-			{
-				$arrFullFillment[$data['ordNo']] =
-
-					[
-								"orderCreatedTime" => gmdate("Y-m-d\TH:i:s\Z", strtotime($data['ordStlEndDt'])),
-								"customerInfo" => [
-									"addressee" => $data["ordNm"],
-									"address1" => $data["rcvrBaseAddr"],
-									"province" => "",
-									"postalCode" => "0", // Y (String) -> from elevenia doesn't have zip code
-									"country" => "Indonesia",
-									"phone" => $data['rcvrPrtblNo'],
-									"email" => "order@elevenia.co.id"
-								],
-								"orderShipmentInfo" => [
-									"addressee" => $data["rcvrNm"],
-									"address1" => $data["rcvrBaseAddr"],
-									"address2" => "",
-									"subDistrict" => "",
-									"district" => "",
-									"city" => "",
-									"province" => "",
-									"postalCode" => "0", // Y (String) -> from elevenia doesn't have zip code
-									"country" => "Indonesia",
-									"phone" => $data["rcvrTlphn"],
-									"email" => "order@elevenia.co.id"
-								],
-								"paymentType" => "NON_COD",
-								"shippingType" => "STANDARD_2_4_DAYS",
-								"grossTotal" => (float)number_format($data['orderAmt'], 2, ".", ""),
-								"currUnit" => "IDR",
-								"orderItems" => null
-
-					];
-
-				$arrFullFillmentItem[$data['ordNo']][] = $data['sellerPrdCd']."|".$data['ordQty']."|".$data['selPrc'];
-			}
-			foreach($arrFullFillmentItem as $key => $data)
-			{
-				$orderItem = array();
-				foreach($data as $keySub => $dataSub)
-				{
-					$arrDataSub = explode("|", $dataSub);
-					$orderItem[] = [
-						"partnerId" => $partnerId,
-						"itemId" => $arrDataSub[0],
-						"qty" => (int)$arrDataSub[1],
-						"subTotal" => (float)$arrDataSub[2]
-					];
-				}
-
-				$arrFullFillment[$key]['orderItems'] = $orderItem;
-			}
-		}
-		else
-		{
-			//1 order 1 item
-			//$order = $order['order'];
 			$orderItem[] = [
 				"partnerId" => $partnerId,
-				"itemId" => $order['sellerPrdCd'],
-				"qty" => (int)$order['ordQty'],
-				"subTotal" => (float)$order['selPrc']
+				"itemId" => $val['sellerPrdCd'],
+				"qty" => (int)$val['ordQty'],
+				"subTotal" => (float)number_format($val['ordQty'] * $val['selPrc'], 2, ".", "")
 			];
-
-			$arrFullFillment =
-				[
-					$order['ordNo'] =>
-					[
-						"orderCreatedTime" => gmdate("Y-m-d\TH:i:s\Z", strtotime($order['ordStlEndDt'])),
-						"customerInfo" => [
-							"addressee" => $order["ordNm"],
-							"address1" => $order["rcvrBaseAddr"],
-							"province" => "",
-							"postalCode" => "0", // Y (String) -> from elevenia doesn't have zip code
-							"country" => "Indonesia",
-							"phone" => $order['rcvrPrtblNo'],
-							"email" => "order@elevenia.co.id"
-						],
-						"orderShipmentInfo" => [
-							"addressee" => $order["rcvrNm"],
-							"address1" => $order["rcvrBaseAddr"],
-							"address2" => "",
-							"subDistrict" => "",
-							"district" => "",
-							"city" => "",
-							"province" => "",
-							"postalCode" => "0", // Y (String) -> from elevenia doesn't have zip code
-							"country" => "Indonesia",
-							"phone" => $order["rcvrTlphn"],
-							"email" => "order@elevenia.co.id"
-						],
-						"paymentType" => "NON_COD",
-						"shippingType" => "STANDARD_2_4_DAYS",
-						"grossTotal" => (float)number_format($order['orderAmt'], 2, ".", ""),
-						"currUnit" => "IDR",
-						"orderItems" => $orderItem
-
-					]
-				];
 		}
 
+		// Convert to elevenia date (Asia/Jakarta) to UTC
+		$orderCreatedTime = new \DateTime($orderElev['ordStlEndDt'], new \DateTimeZone('Asia/Jakarta'));
+		$orderCreatedTime->setTimezone(new \DateTimeZone('UTC'));
 
-		return $arrFullFillment;
+		return [
+			"orderCreatedTime" => new \MongoDate($orderCreatedTime->getTimestamp()),
+			"customerInfo" => [
+				"addressee" => $orderElev["ordNm"],
+				"address1" => $orderElev["rcvrBaseAddr"],
+				"province" => "",
+				"postalCode" => "0", // Y (String) -> from elevenia doesn't have zip code
+				"country" => "Indonesia",
+				"phone" => $orderElev['rcvrPrtblNo'],
+				"email" => "order@elevenia.co.id"
+			],
+			"orderShipmentInfo" => [
+				"addressee" => $orderElev["rcvrNm"],
+				"address1" => $orderElev["rcvrBaseAddr"],
+				"address2" => "",
+				"subDistrict" => "",
+				"district" => "",
+				"city" => "",
+				"province" => "",
+				"postalCode" => "0", // Y (String) -> from elevenia doesn't have zip code
+				"country" => "Indonesia",
+				"phone" => $orderElev["rcvrTlphn"],
+				"email" => "order@elevenia.co.id"
+			],
+			"paymentType" => "NON_COD",
+			"shippingType" => "STANDARD_2_4_DAYS",
+			"grossTotal" => (float)number_format($orderElev['orderAmt'], 2, ".", ""),
+			"currUnit" => "IDR",
+			"orderItems" => $orderItem
+
+		];
 	}
 
-	/*
-    public function parseOrder($partnerId, $order)
-
-    {
-		$orderItem = [];
-		if(isset($order[0])) {
-			$totAmount = 0;
-			foreach($order as $itemOrder)
-			{
-				$orderItem[] = [
-					"partnerId" => $partnerId,
-					"itemId" => $itemOrder['sellerPrdCd'],
-					"qty" => (int)$itemOrder['ordQty'],
-					"subTotal" => (float)$itemOrder['selPrc']
-				];
-				$totAmount = $totAmount + (int)$itemOrder['selPrc'];
-			}
-			$order = $order[0];
-			$order['orderAmt'] = $totAmount;
-		} else {
-			$orderItem[] = [
-				"partnerId" => $partnerId,
-				"itemId" => $order['sellerPrdCd'],
-				"qty" => (int)$order['ordQty'],
-				"subTotal" => (float)$order['selPrc']
-			];
-		}
-
-        $orders = [
-            "orderCreatedTime" => gmdate("Y-m-d\TH:i:s\Z", strtotime($order['ordStlEndDt'])),
-            "customerInfo" => [
-                "addressee" => $order["ordNm"],
-                "address1" => $order["rcvrBaseAddr"],
-                "province" => "",
-                "postalCode" => "0",
-                "country" => "Indonesia",
-                "phone" => $order['rcvrPrtblNo'],
-                "email" => "order@elevenia.co.id"
-            ],
-            "orderShipmentInfo" => [
-                "addressee" => $order["rcvrNm"],
-                "address1" => $order["rcvrBaseAddr"],
-                "address2" => "",
-                "subDistrict" => "",
-                "district" => "",
-                "city" => "",
-                "province" => "",
-                "postalCode" => "0",
-                "country" => "Indonesia",
-                "phone" => $order["rcvrTlphn"],
-                "email" => "order@elevenia.co.id"
-            ],
-            "paymentType" => "NON_COD",
-            "shippingType" => "STANDARD_2_4_DAYS",
-            "grossTotal" => (float)number_format($order['orderAmt'], 2, ".", ""),
-            "currUnit" => "IDR",
-            "orderItems" => $orderItem
-        ];
-
-        return $orders;
-    }
-	*/
-
-    private function eleveniaDate($date)
+	private function eleveniaDate($date)
 	{
 		return str_replace('-', '/', $date);
 	}
@@ -354,32 +214,28 @@ class Order
 		return $parsedOrders;
 	}
 
-	public function save($partnerId, $order)
+	public function save($partnerId, $orderElevenia)
 	{
-		// denormalize order
-		$orderRegional = $this->parseOrder($partnerId, $order);
-		$arrSalesOrderRegional = [];
-		foreach ($orderRegional as $keyRes => $itemRes) {
-			$arrSalesOrderRegional[] =
+		$orderCmps = $this->parseOrderFromEleveniaToCmps($partnerId, $orderElevenia);
+		$order = [
+			"orderId" => $orderElevenia['ordNo'],
+			"partnerId" => $partnerId,
+			"channel" =>
 				[
-					"orderId" => $keyRes,
-					"channel" =>
-						[
-							"name" => "elevenia",
-							"order" => $order,
-							"last_sync" => date("Y-m-d H:i:s")
-						],
-					"acommerce" =>
-						[
-							"order" => $itemRes,
-							"last_sync" => date("Y-m-d H:i:s")
-						],
-					"status" => "NEW",
-					"created_date" => date("Y-m-d H:i:s"),
-					"updated_date" => ""
-				];
-		}
+					"name" => "elevenia",
+					"order" => $orderElevenia,
+					"last_sync" => new \MongoDate()
+				],
+			"acommerce" =>
+				[
+					"order" => $orderCmps,
+					"last_sync" => new \MongoDate()
+				],
+			"status" => "NEW",
+			"createdDate" => new \MongoDate(),
+			"updatedDate" => new \MongoDate()
+		];
 
-		return SalesOrder::raw()->insert($arrSalesOrderRegional);
+		return SalesOrder::raw()->insert($order);
 	}
 }
