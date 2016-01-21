392013476206e540ec63f16ed11baa8ab00f06de
Ivan <ivan.petrus@acommerce.asia>
2016-01-21 15:25:49 +0700

    Update GetNewSalesOrdersTest

--- tests/GetNewSalesOrdersTest.php
+++ tests/GetNewSalesOrdersTest.php
@@ -4,7 +4,7 @@ class GetNewSalesOrdersTest extends TestCase
 {
     public function testJobs()
     {
-        $this->expectsJobs(App\Jobs\GetPartnerNewSalesOrdersFromChannel::class);
+        $this->expectsJobs(\App\Jobs\GetSalesOrdersFromChannel::class);
 
         Artisan::call('salesorder:create');
     }
